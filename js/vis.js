a7880f83eaf7d9641602a0b8624dce3989e58939
emilyfuhrman <emily.c.fuhrman@gmail.com>
2015-11-29 21:28:00 -0500

    adding comments

--- js/vis.js
+++ js/vis.js
@@ -2,38 +2,60 @@ var init = function(){
 
 	return {
 
+		//empty loading array to hold pending datasets
 		loading:[],
 
+		//store screen height and width
 		width:window.innerWidth,
 		height:window.innerHeight,
 
 		projection:null,
 		path:null,
 
+		//ensures the callback function is only called
+		//once all datasets have been retrieved
 		loadingManager:function(_elem,_callback){
 			var self = vis;
 			var empty = false;
+
+			//remove name of retrieved JSON document from loading array
 			self.loading = self.loading.filter(function(d){
 				return d !== _elem;
 			});
+
+			//if loading array is empty, callback function
 			if(self.loading.length === 0){ 
 				_callback();
 			}
 		},
+
+		//retrieves datasets
 		getData:function(_callback){
 			var self = vis;
+
+			//'datasets' array holds strings for all files to be retrieved
 			var datasets = ['countries'],
 				callback = _callback;
 
+			//cycle through each string in the array
 			datasets.forEach(function(d){
-				self.loading.push(d);
-			});
 
-			d3.json('data/countries.json',function(e,d){
-				self.countries = d;
-				self.loadingManager('countries',callback);
+				//concat filename and path from dataset name
+				var filename = d +'.json',
+					filepath = 'data/' +filename;
+
+				//push name of dataset into loading array
+				self.loading.push(d);
+
+				//retrieve dataset
+				d3.json(filepath,function(e,d){
+					self.countries = d;
+					self.loadingManager('countries',callback);
+				});
 			});
 		},
+
+		//generates visualization
 		generate:function(){
 			var self = vis;
 
@@ -60,6 +82,8 @@ var init = function(){
 				.attr('d',self.path);
 			map.exit().remove();
 		},
+
+		//puts data in correct format
 		processData:function(){
 			var self = vis;
 			self.generate();
