8c180c8d37521a2d05c48dc1edde6dee1754488b
Bill Kennedy <bill@intelligentmachines.ca>
2017-03-23 19:04:32 -0400

    Roughed out template and nav UI

---
+++ js/ui.js
@@ -0,0 +1,25 @@
+class UI {
+	
+  constructor(){
+   
+  }
+  
+  initPanels(){
+        
+    var tabs = document.getElementById("tabs").children;
+    var panels = document.getElementById("panels").children;
+    
+    for(var i=0;i<tabs.length;i++) {
+      tabs[i].setAttribute('data-panel',i);
+      tabs[i].onclick = function() {
+        for(var j=0;j<panels.length;j++) {
+          panels[j].style.display = 'none';
+        }
+        panels[this.getAttribute('data-panel')].style.display = 'block';
+      }
+    }
+  }
+}
+
+var ui = new UI;
+ui.initPanels();
